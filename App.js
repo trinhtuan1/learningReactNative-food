91084e67f6cf56144a8365e4f89f23757521cf55
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-09 16:46:59 +0700

    add SearchScreen.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
