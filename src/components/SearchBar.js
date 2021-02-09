74f8a0b578dbdfe7d92b87b55daab08b0ef9bf5b
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-09 17:40:26 +0700

    modify SearchBar.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+
+const SearchBar = () => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather
+        name="search"
+        style={styles.iconStyle}
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 15,
+    backgroundColor: '#f0eeee',
+    height: 50,
+    marginHorizontal: 15,
+    borderRadius: 5,
+    flexDirection: 'row'
+  },
+  iconStyle: {
+    fontSize: 35,
+    marginHorizontal: 15,
+    alignSelf: 'center',
+  },
+  inputStyle: {
+    fontSize: 18,
+    flex: 1,
+  }
+});
+
+export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   return (
-    <View>
+    <View style={styles.backgroundStyle}>
+      <SearchBar />
       <Text>Search Screen</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  backgroundStyle: {
+    backgroundColor: '#fff',
+  }
 });
 
 export default SearchScreen;
