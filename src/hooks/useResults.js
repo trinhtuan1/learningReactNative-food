5b2d9f0431275e802727b7e9755c54385656f732
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-16 20:22:55 +0700

    add ResultsList.js

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -10,7 +10,7 @@ export default useResults = () => {
     try {
       const response = await yelp.get('/search', {
         params: {
-          limit: 10,
+          limit: 30,
           term: searchTerm,
           location: 'san jose'
         }
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,11 +3,18 @@ import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
   const [ term, setTerm ] = React.useState('');
   const [ searchApi, results, errorMessage ] = useResults();
    
+  const filterResultByPrice = price => {
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
+
   return (
     <View style={styles.backgroundStyle}>
       <SearchBar 
@@ -19,6 +26,18 @@ const SearchScreen = () => {
         errorMessage ? <Text>{errorMessage}</Text> : null
       }
       <Text>We have found {results.length} results.</Text>
+      <ResultsList 
+        title="Cost Effective" 
+        result={filterResultByPrice('$')}
+      />
+      <ResultsList 
+        title="Bit Pricer" 
+        result={filterResultByPrice('$$')}
+      />
+      <ResultsList 
+        title="Big Spender"
+        result={filterResultByPrice('$$$')}
+      />
     </View>
   );
 };
