c51eede9e4778abe2d2ab11c398c1fd3a843c998
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-16 19:48:19 +0700

    add useResults.js

---
+++ src/hooks/useResults.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import yelp from '../api/yelp';
+
+export default useResults = () => {
+  const [ results, setResults ] = React.useState([]);
+  const [ errorMessage, setErrorMessage ] = React.useState('');
+
+  const searchApi = async (searchTerm) => {
+    console.log('Hi there');
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 10,
+          term: searchTerm,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+      setErrorMessage('');
+    } catch(err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  React.useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
+  return [ searchApi, results, errorMessage ];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,33 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [ term, setTerm ] = React.useState('');
-  const [ results, setResults ] = React.useState([]);
-  const [ errorMessage, setErrorMessage ] = React.useState('');
-
-  const searchApi = async (searchTerm) => {
-    console.log('Hi there');
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 30,
-          term: searchTerm,
-          location: 'san jose'
-        }
-      });
-      setResults(response.data.businesses);
-      setErrorMessage('');
-    } catch(err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  React.useEffect(() => {
-    searchApi('pasta');
-  }, []);
-
+  const [ searchApi, results, errorMessage ] = useResults();
+   
   return (
     <View style={styles.backgroundStyle}>
       <SearchBar 
