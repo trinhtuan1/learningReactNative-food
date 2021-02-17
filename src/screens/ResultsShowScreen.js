c25cf412d568a1e75e5b67771280449aee3b63c0
trinhtuan <trinhminhtuan.6868@gmail.com>
2021-02-17 15:11:30 +0700

    add fetch api in ResultsShowScreen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [ result, setResult ] = React.useState('');
   const id = navigation.getParam('id');
-  console.log(id);
+
+  console.log(result);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`${id}`);
+    setResult(response.data);
+  };
+
+  React.useEffect(() => {
+    getResult(id);
+  }, []);
 
   return (
     <View>
